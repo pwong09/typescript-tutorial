53547e49ce6021e3a115526f69bc1061bdbb983f
Pearl Wong <pearlw@adobe.com>
2022-08-02 15:01:46 -0700

    add collection types

---
+++ 03-collection-types.ts
@@ -0,0 +1,13 @@
+// Arrays
+let list: number[] = [1, 2, 3];
+
+let anotherList: Array<number> = [4, 5, 6];
+
+// Tuples - can hold mixed types
+// variableName: [type, type, ...]
+
+let person1: [string, number] = ['Marcia', 35];
+// below will throw an error
+// let person2: [string, number] = ["Peter", 25, true];
+
+// order of values must match order of types
