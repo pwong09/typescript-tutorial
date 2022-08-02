4e5c3cf99f171980b5e477061846e864ac754586
Pearl Wong <pearlw@adobe.com>
2022-08-02 14:46:24 -0700

    reorganize file names

--- build/02-variable-types.js
+++ build/02-variable-types.js
@@ -1,7 +1,4 @@
-function addNumbers(x, y) {
-    return x + y;
-}
-console.log(addNumbers(3, 6));
+"use strict";
 // declaring variable types
 // enum - symbolic name for set of values
 // treated as data types
@@ -12,7 +9,7 @@ var ContractStatus;
     ContractStatus[ContractStatus["Temp"] = 2] = "Temp";
     ContractStatus[ContractStatus["Apprentice"] = 3] = "Apprentice";
 })(ContractStatus || (ContractStatus = {}));
-var employeeStatus = ContractStatus.Temp;
+let employeeStatus = ContractStatus.Temp;
 console.log(employeeStatus); // returns 2
 console.log(ContractStatus[employeeStatus]); // returns Temp
 // any & unknown types
@@ -20,8 +17,8 @@ console.log(ContractStatus[employeeStatus]); // returns Temp
 // useful when you expect a value from 3rd party lib
 // or user inputs where value is dynamic
 // any allows you to reassign diff types of values
-var anyValue = 10;
-anyValue = 'Mateo';
+let anyValue = 10;
+anyValue = "Mateo";
 anyValue = true;
 // allows a property that doesn't exist for the type
 // can call randomValue as a function
@@ -34,23 +31,24 @@ anyValue = true;
 // any value is assignable to type unknown
 // can't access any properties of an unknown type
 // can't call or construct unknown type
-var unknownValue = 10;
+let unknownValue = 10;
 unknownValue = true;
-unknownValue = 'Pearl';
+unknownValue = "Pearl";
 // console.log(unknownValue.name) // Error: Object is of type unknown
 // unknownValue(); // Error: Object is of type unknown
 // unknownValue.toUpperCase(); // Error: Object is of type unknown
-// The core difference between any and unknown is 
-// you are unable to interact with a variable of type unknown; 
+// The core difference between any and unknown is
+// you are unable to interact with a variable of type unknown;
 // doing so generates a compiler error.
-// any bypasses any compile - time checks, and the object is 
-// evaluated at runtime; if the method or property exists 
+// any bypasses any compile - time checks, and the object is
+// evaluated at runtime; if the method or property exists
 // it will behave as expected.
 // type assertion - keyword as
-var randomValue = 10;
+let randomValue = 10;
 randomValue = true;
 randomValue = "Mateo";
-if (typeof randomValue === "string") { // type guard
+if (typeof randomValue === "string") {
+    // type guard
     console.log(randomValue.toUpperCase()); //* Returns MATEO to the console.
 }
 else {
@@ -59,17 +57,28 @@ else {
 // union types
 // restricts assignment of values to specified types
 // uses vertical bar | to separate each type
-var multiType;
+let multiType;
 multiType = 20;
 multiType = true;
 // multiType = "string"; // invalid
 function add(x, y) {
-    if (typeof x === 'number' && typeof y === 'number')
+    if (typeof x === "number" && typeof y === "number")
         return x + y;
-    if (typeof x === 'string' && typeof y === 'string')
+    if (typeof x === "string" && typeof y === "string")
         return x.concat(y);
-    throw new Error('Parameters must be same type as each other: numbers or strings');
+    throw new Error("Parameters must be same type as each other: numbers or strings");
 }
-console.log(add('one', 'two'));
+console.log(add("one", "two"));
 console.log(add(1, 2));
-console.log(add('one', 2));
+let newManager = {
+    employeeID: 12345,
+    age: 34,
+    stockPlan: true
+};
+let myResult;
+myResult = "incomplete";
+myResult = "pass";
+let diceRoll;
+diceRoll = 1;
+diceRoll = 2;
+// diceRoll = 7; // invalid
