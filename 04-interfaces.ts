977361dd276808a4c366eb761d0c0830c0a67cb6
pwong09 <pwong09@gmail.com>
2022-08-18 09:42:44 -0700

    interface practice

--- 04-interfaces.ts
+++ 04-interfaces.ts
@@ -38,6 +38,7 @@ let employee: ExampleEmployee = {
 // --> you can catch errors & make sure
 // --> you're passing the right parameters at compile time 
 // --> instead of at run time
+// Optional properties may be omitted without raising an error.
 
 // can use interface to:
 // 1. Create shorthand names for commonly used types
@@ -83,3 +84,44 @@ let newIceCream: anotherIceCreamArray;
 newIceCream = ['chocolate', 'vanilla', 'strawberry'];
 let myString: string = newIceCream[0];
 console.log(myString); // consoles chocolate
+
+// by using an interface, you can define your own array type
+// anyone who wants to work with that interface can apply it consistently
+
+/////////////////////////////////
+/*---- Interfaces & API ------*/
+///////////////////////////////
+
+// user interface to describe existing JS APIs and
+// clarify function parameters & return types
+// example
+
+const fetchURL = 'https://jsonplaceholder.typicode.com/posts'
+// Interface describing the shape of our json data
+
+interface Post {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+}
+async function fetchPosts(url: string) {
+    let response = await fetch(url);
+    let body = await response.json();
+    return body as Post[];
+}
+async function showPost() {
+  let posts = await fetchPosts(fetchURL);
+  // Display the contents of the first item in the response
+  let post = posts[0];
+  console.log("Post #", post.id);
+  // If the userId is 1, then display a note that it's an administrator
+  console.log(
+    "Author: " + (post.userId === 1 ? "Administrater" : post.userId.toString())
+  );
+  console.log("Title: " + post.title);
+  console.log("Body: ", post.body);
+}
+
+showPost();
+
--- build/04-interfaces.js
+++ build/04-interfaces.js
@@ -1,6 +1,15 @@
 "use strict";
 // type checking focuses on shpae that values have
 // "duck typing" or "structural subtyping"
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 // interface doesn't init or implement the properties declared within
 // only job is to describe a type
 // defines what code contract requires
@@ -15,10 +24,44 @@ let employee = {
 };
 let myIceCream = {
     flavor: "strawberry",
-    scoops: 2
+    scoops: 2,
+    instructions: 'Cup not cone, no toppings'
 };
 console.log(myIceCream.flavor);
 function tooManyScoops(dessert) {
     return dessert.scoops >= 7 ? dessert.scoops + ' is too many scoops!' : 'Your order will be ready soon!';
 }
-console.log(tooManyScoops({ flavor: 'vanilla', scoops: 5 }));
+console.log(tooManyScoops({ flavor: 'vanilla', scoops: 5, instructions: "everything" }));
+let newIceCream;
+newIceCream = ['chocolate', 'vanilla', 'strawberry'];
+let myString = newIceCream[0];
+console.log(myString); // consoles chocolate
+// by using an interface, you can define your own array type
+// anyone who wants to work with that interface can apply it consistently
+/////////////////////////////////
+/*---- Interfaces & API ------*/
+///////////////////////////////
+// user interface to describe existing JS APIs and
+// clarify function parameters & return types
+// example
+const fetchURL = 'https://jsonplaceholder.typicode.com/posts';
+function fetchPosts(url) {
+    return __awaiter(this, void 0, void 0, function* () {
+        let response = yield fetch(url);
+        let body = yield response.json();
+        return body;
+    });
+}
+function showPost() {
+    return __awaiter(this, void 0, void 0, function* () {
+        let posts = yield fetchPosts(fetchURL);
+        // Display the contents of the first item in the response
+        let post = posts[0];
+        console.log("Post #", post.id);
+        // If the userId is 1, then display a note that it's an administrator
+        console.log("Author: " + (post.userId === 1 ? "Administrater" : post.userId.toString()));
+        console.log("Title: " + post.title);
+        console.log("Body: ", post.body);
+    });
+}
+showPost();
