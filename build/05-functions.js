39be60ed43bee52aefc2d5e85c75bf7c6641ba5c
pwong09 <pwong09@gmail.com>
2022-08-19 15:31:04 -0700

    function parameters

--- build/05-functions.js
+++ build/05-functions.js
@@ -1,18 +1,16 @@
+"use strict";
 // simplifies development of functions
 // make them easier to troubleshoot
 // enable type parameters and return values
 // can make parameters required (assumed) or optional
-
-function addNumbersTwo(x: number, y: number): number {
+function addNumbersTwo(x, y) {
     return x + y;
 }
 console.log(addNumbersTwo(1, 2));
-
-
 // contextual typing
 // total's type is inferred 
-let total = function (input: number[]): number {
-    let total: number = 0;
+let total = function (input) {
+    let total = 0;
     for (let i = 0; i < input.length; i++) {
         if (isNaN(input[i])) {
             continue;
@@ -20,66 +18,54 @@ let total = function (input: number[]): number {
         total += Number(input[i]);
     }
     return total;
-}
-
+};
 // using implicit return
-let addNumbersThree = (x: number, y: number): number => x + y;
-
-function displayAlert(message: string) {
-  alert("The message is " + message);
+let addNumbersThree = (x, y) => x + y;
+function displayAlert(message) {
+    alert("The message is " + message);
 }
-
-function sum(input: number[]): number {
-  let total = 0;
-  for (let count = 0; count < input.length; count++) {
-    if (isNaN(input[count])) {
-      continue;
+function sum(input) {
+    let total = 0;
+    for (let count = 0; count < input.length; count++) {
+        if (isNaN(input[count])) {
+            continue;
+        }
+        total += Number(input[count]);
     }
-    total += Number(input[count]);
-  }
-  return total;
+    return total;
 }
-
 console.log(sum([1, 2, 3]));
 // sum([1, "two", 3]); // error - TS checks each element in array
-
 // define optional parameters by appending a question mark 
 // (?) to the end of the parameter name
-
-function sumAgain(x: number, y?: number): number {
+function sumAgain(x, y) {
     if (y === undefined) {
         return x;
-    } else {
+    }
+    else {
         return x + y;
     }
 }
-
 sumAgain(1, 2); // Returns 3
-sumAgain(1);    // Returns 1
-
+sumAgain(1); // Returns 1
 // can assign default values to optional parameters
 // default parameters come after required parameters
-
-function anotherSum(x: number, y = 25): number {
+function anotherSum(x, y = 25) {
     return x + y;
 }
-
 anotherSum(1, 2); // returns 3
 anotherSum(1); // returns 26
-
 // rest parameters are boundless number of optional paremters
-
-function addAllNumbers(x: number, ...args: number[]): number {
-    let total: number = x;
+function addAllNumbers(x, ...args) {
+    let total = x;
     for (let counter = 0; counter < args.length; counter++) {
         if (isNaN(args[counter])) {
             continue;
         }
-        total += Number(args[counter]);  
+        total += Number(args[counter]);
     }
     return total;
 }
-
-addAllNumbers(1, 2, 3, 4, 5, 6, 7);  // returns 28
-addAllNumbers(2);                    // returns 2
+addAllNumbers(1, 2, 3, 4, 5, 6, 7); // returns 28
+addAllNumbers(2); // returns 2
 // addAllNumbers(2, 3, "three");        // flags error due to data type at design time, returns 5
